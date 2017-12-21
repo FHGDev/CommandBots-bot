134f7f495b063e1ed44375324069ab90c4204382
FHGDev <gideon.lingle14@gmail.com>
2017-12-21 13:06:40 -0600

    Update code.js

--- code.js
+++ code.js
@@ -10,25 +10,16 @@ client.on('message', message => {
   if (message.content === prefix +'ping') {
     message.reply('pong');
 
-	}
-});
-
-client.on('message', message => {
-  // If the message is "what is my avatar"
+}
+	 // If the message is "what is my avatar"
   if (message.content === prefix + 'what is my avatar') {
     // Send the user's avatar URL
     message.reply(message.author.avatarURL);
   }
-});
-
-client.on('message', message => {
-if (message.content === prefix + 'foo') {
+	if (message.content === prefix + 'foo') {
 		message.channel.sendMessage("bar!");
-	} 
- });
-
-client.on('message', message => {
-if (message.content === prefix + 'ban') {
+	}
+	if (message.content === prefix + 'ban') {
 	if (!message.member.permissions.has('ADMINISTRATOR')) return message.reply('you do not have permission to access that command.');
 	const member = message.mentions.members.first();
 	if (!member) return message.reply('Invalid usage, please do \`c!ban @user\`');
